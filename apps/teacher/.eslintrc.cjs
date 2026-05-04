545007db32e6793b4957eddf1ed2aad861ec637d
Victor Ruiz <veruiz@uc.cl>
2026-05-04 12:14:10 -0400

    refactor(eslint): remove unnecessary configurations from practice and teacher ESLint files

--- apps/teacher/.eslintrc.cjs
+++ apps/teacher/.eslintrc.cjs
@@ -1,9 +1,3 @@
 module.exports = {
     extends: ['next/core-web-vitals'],
-    root: true,
-    parser: '@typescript-eslint/parser',
-    rules: {
-        'react/react-in-jsx-scope': 'off',
-        '@typescript-eslint/no-unused-vars': ['warn', { argsIgnorePattern: '^_' }],
-    },
 }
