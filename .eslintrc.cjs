b49f53fd15528d09e9487c184ca6f0b5edf13f60
Victor Ruiz <veruiz@uc.cl>
2026-05-04 12:09:21 -0400

    feat(eslint): update ESLint configuration to enhance TypeScript support and add specific rules for TypeScript files

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -5,8 +5,10 @@ module.exports = {
         node: true,
         es2022: true,
     },
+    parser: '@typescript-eslint/parser',
     extends: [
         'eslint:recommended',
+        'plugin:@typescript-eslint/recommended',
         'plugin:react/recommended',
         'plugin:react-hooks/recommended'
     ],
@@ -22,7 +24,18 @@ module.exports = {
             version: 'detect',
         },
     },
+    overrides: [
+        {
+            files: ['*.ts', '*.tsx'],
+            parser: '@typescript-eslint/parser',
+            rules: {
+                '@typescript-eslint/no-unused-vars': ['warn', { argsIgnorePattern: '^_' }],
+                '@typescript-eslint/no-explicit-any': 'warn',
+            },
+        },
+    ],
     rules: {
         'no-unused-vars': ['warn', { argsIgnorePattern: '^_' }],
+        'react/react-in-jsx-scope': 'off',
     },
 }
